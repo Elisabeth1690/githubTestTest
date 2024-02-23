46df6556b48a9621701dd25aa4220435f02a85f2
Elisabeth1690 <90ebaa@gmail.com>
2024-02-23 11:15:38 +0100

    har lagt inn alle knappene og div conteinere

--- harrypotter.js
+++ harrypotter.js
@@ -1,7 +1,23 @@
+const griffendorBtn = document.getElementById("griffindor-Btn");
+const slytherinBtn = document.getElementById("slytherin-Btn");
+const huffelpuffBtn = document.getElementById("huffelpuff-Btn");
+const ravenclawBtn = document.getElementById("ravenclaw-Btn");
+
+const griffindorConteiner = document.getElementById("griffindor-Conteiner");
+const slytherinConteiner = document.getElementById("slytherin-Conteiner");
+const huffelpuffConteiner = document.getElementById("huffelpuff-Conteiner");
+const ravenclawConteiner = document.getElementById("ravenclaw-Conteiner");
+
+griffendorBtn.addEventListener("click", getGriffendorHouse);
+
 async function fetchStaff() {
   const hogwartsStaffRequwst = await fetch(
     "https://hp-api.onrender.com/api/characters/staff"
   );
   let result = await hogwartsStaffRequwst.json();
   return result;
+}
+
+function getGriffendorHouse() {
+  alert("heihei");
 }
