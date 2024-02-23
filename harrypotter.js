f11ec64110230d0d250ff3702f7e6b350ff0f20a
Elisabeth1690 <90ebaa@gmail.com>
2024-02-23 14:04:00 +0100

    må øve mer, ingen ting funker

--- harrypotter.js
+++ harrypotter.js
@@ -7,17 +7,43 @@ const griffindorConteiner = document.getElementById("griffindor-Conteiner");
 const slytherinConteiner = document.getElementById("slytherin-Conteiner");
 const huffelpuffConteiner = document.getElementById("huffelpuff-Conteiner");
 const ravenclawConteiner = document.getElementById("ravenclaw-Conteiner");
+let griffindorHouse;
+let griffindorHouseArray;
+//let slytherinHouseArray;
+//let huffelpuffHouseArray;
+//let ravenclawHouseArray;
 
-griffendorBtn.addEventListener("click", getGriffendorHouse);
+griffendorBtn.addEventListener("click", fetchGriffendor);
 
-async function fetchStaff() {
-  const hogwartsStaffRequwst = await fetch(
-    "https://hp-api.onrender.com/api/characters/staff"
+async function fetchGriffendor() {
+  const griffindorRequwst = await fetch(
+    "https://hp-api.onrender.com/api/characters/students"
   );
-  let result = await hogwartsStaffRequwst.json();
+  let result = await griffindorRequwst.json();
   return result;
 }
-
-function getGriffendorHouse() {
-  alert("heihei");
+console.log(fetchGriffendor());
+async function getGriffendorHouse() {
+  try {
+    griffindorHouse = await fetchGriffendor();
+    let griffindorHouseDoorman = griffindorHouse.filter(
+      (griffindorHouse) =>
+        griffindorHouse.house === "Gryffindor" &&
+        griffindorHouse.hogwartsStudent === true
+    );
+    griffindorHouseArray.push(griffindorHouseDoorman);
+    console.log(griffindorHouseArray);
+    showAllGriffendor();
+  } catch (error) {
+    console.error("Ops klarte ikke å laste ned hogwarts staff", error);
+  }
+}
+console.log(griffindorHouseDoorman);
+function showAllGriffendor() {
+  griffindorConteiner.innerHTML = "";
+  griffindorHouse.forEach((student, index) => {
+    const griffendorStudent = document.createElement("div");
+    griffendorStudent.style.display = "flex";
+    griffendorStudent.style.flexDirection = "column";
+  });
 }
